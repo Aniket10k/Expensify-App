0f92ad79a5c6f53045ee2f388a693ca137fbfb5e
Aniket10k <kulkarni10aniket@gmail.com>
2021-03-02 23:18:53 +0530

    Updated configs

--- src/components/expenseListItem.js
+++ src/components/expenseListItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItems = ({ id, description, amount, createdAt }) => {
     return (
         <div>
             <Link to={`/edit/${id}`}><h2>{description}</h2></Link>
-            <h4>Amount : {amount}</h4>
+            <h4>Amount : {numeral(amount/100).format("$0,0.00")}</h4>
             <h5>Date : {moment(createdAt).format("Do MM, YYYY")}</h5>
         </div>
     );
